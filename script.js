8f89033e97df4c54a3e4efecd81d728d39ff1856
Shoryasingh2376 <singhshorya2376.com>
2026-01-21 10:16:41 +0530

    first code

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+// const para=document.getElementById("para");
+// para[0].textContent="This is updated one"
+// para[1].style.color='green'
+
+
+// const para=document.getElementsByClassName("para");
+// para[0].textContent="This is updated one"
+// para[1].style.color='green'
+
+// const para=document.querySelectorAll(".para");
+// para[0].textContent="This is updated one"
+// para[1].style.color='green'
+
+// const para=document.querySelectorAll(".para");
+// para[0].textContent="This is updated one"
+// para[1].style.color='green'
+
+// const container=document.quartSelector(".container")
+// container.innerHTML="<h1> This ia a heading<h1>"
+// function popup(event){
+//     console.log(event.key)
+//     alert("You have clicked the button")
+// }
+
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// btn.addEventListener('click',popop)
+// btn.addEventListener('mouseover',popop)
+
+
+// const button=document.getElementById("btn")
+// const StopButton=document.getElementById("stop")
+
+// button.addEventListener('click',showMessage)
+
+// StopButton.addEventListener('click',function(){
+//     button.removeEventListener('click',showMessage)
+// })
+
+// function popup(event){
+//     console.log(event.key)
+//     alert("You have clicked the button")
+// }
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// btn.addEventListener('keyup', popup)
+
+
+// const form=document.querySelector("form")
+// form.addEventListener('submit',function(event){
+//     event.preventDefault()
+//     console.log("Form Submited")
+// }
+// )
+
+
+// const div=document.querySelector(".container")
+// const div2=document.querySelector(".outer") 
+// const btn=document-querySelector ("button")
+// div.addEventListener(" click", ()=>{console.1og("Div")},true) 
+// div2.addEventListener("click", ()=>{console.1og("Div2")},true) 
+// btn.addEventListener("click", ()=>{console.1og( "Button")},true)
